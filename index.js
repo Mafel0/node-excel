ddbd730e492fc0ac8e83bbef1c30e3447cb6df12
Matheus Felipe <108841252+Mafel0@users.noreply.github.com>
2023-09-25 18:00:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const excelJs = require("exceljs");
+const jsonCases = require("./pagina"); //Pagina da varíavel com o Json dos casos
 var fs = require("fs");
 
 const app = express();
@@ -10,24 +11,24 @@ app.get("/export", async (req, res) => {
     try {
         let workbook = new excelJs.Workbook()
 
-        const sheet = workbook.addWorksheet("relatorios")
+        const sheet = workbook.addWorksheet("relatorio")
         sheet.columns = [
-            {header: "Agente", key:"agente", width: 50},
-            {header: "Logradouro", key:"logradouro", width: 50},
-            {header: "N° da residência", key:"numero", width: 100},
-            {header: "Bairro", key:"bairro", width: 50},
-            {header: "Nível da situação", key:"nivel", width: 50}
+            {header: "Latitude", key:"latitude", width: 50},
+            {header: "Longitude", key:"longitude", width: 50},
+            {header: "Status", key:"status", width: 50},
+            {header: "ID do usuário", key:"user_id", width: 50},
+            {header: "Data", key:"caseData", width: 50},
+            {header: "Complemento", key:"complement", width: 50}
         ]
 
-        let object = JSON.parse(fs.readFileSync('relatorios.json', 'utf8'))
-
-        await object.relatorios.map((value, idx) => {
+        await jsonCases.forEach((value, idx) => {
             sheet.addRow({
-                agente: value.agente,
-                logradouro: value.logradouro,
-                numero: value.numero,
-                bairro: value.bairro,
-                nivel: value.nivel
+                latitude: value.latitude,
+                longitude: value.longitude,
+                status: value.status,
+                user_id: value.user_id,
+                caseData: value.caseData,
+                complement: value.complement
             });
         });
 
@@ -37,12 +38,13 @@ app.get("/export", async (req, res) => {
         );
         res.setHeader(
             "Content-Disposition",
-            "attachment;filename=" + "relatorios.xls"
+            "attachment;filename=" + "relatorio.xls"
         );
 
         workbook.xlsx.write(res)
     } catch (error) {
-        console.log(error)
+        console.log(error);
+        res.status(500).json({ error: 'Erro ao exportar para Excel.' });
     }
 })
 
